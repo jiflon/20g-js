8232f6b33c47aa0d837b7d0bab7ddabe40379cd4
Ferdinand Bracho <ferdinand.bracho@gmail.com>
2022-06-29 22:13:13 -0500

    Clase 1

---
+++ js/base.js
@@ -0,0 +1,58 @@
+// ! Comentarios en Js
+// Esto es un comentario
+
+/*
+    Esto es 
+    un 
+    comentario
+    multilinea
+    */
+
+// ! Datos en Js
+    // Identificador
+    // Reglas o buenas practicas
+    // Reglas: No deben contener caracteres especial (, ! @) -> let @@@ = 'test'
+    // Regla: No incian con digitos
+        // let 9varibale = 9; -> Error
+    // Reglas: No acepta palabra reservadas 
+        // let else = 'else' => Error
+    // Buena practica; camelCase 99% - PascalCase -> Clases - CONSTANTE 
+    // Buena practica; Naming descriptivo  -> let estudiantesKodemia = 'koders'
+    // Consideracion; Sensible a las mayus y minis 
+
+
+// ! Variables y Constantes 
+    // Variables = Mutabilidad 
+    // Constantes = No cambia 
+
+    // Variables - palabras reservadas para declarar; var y let 
+
+    // let edadKoder = 25;
+    // var alturaKoder = 165
+
+    // var alturaKoder = 185
+    // edadKoder =  20
+
+
+    // let edadKoder = 30
+
+    // palabraReservada -> Identificador ->  operadorAsigancion ->  Valor
+    // let test = 1
+    // test = 2
+    // test = 9
+    // console.log(test) 
+
+
+    // console.log(edadKoder);
+    // console.log(alturaKoder);
+
+    // Recomendacion/ Buena practica siempre usar Let 
+
+
+    console.log('a');
+
+
+
+    
+
+    
